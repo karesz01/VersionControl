6f9b3fb9df855c733fd80795db88372e4a17fe17
fbxae3 <fbxae3@SZA0032057>
2022-12-09 14:28:26 +0100

    3

--- gyak_10/gyak_10/Form1.Designer.cs
+++ gyak_10/gyak_10/Form1.Designer.cs
@@ -29,13 +29,34 @@ namespace gyak_10
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(392, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- gyak_10/gyak_10/Form1.cs
+++ gyak_10/gyak_10/Form1.cs
@@ -20,6 +20,9 @@ namespace gyak_10
         int nbrOfSteps = 10;
         int nbrOfStepsIncrement = 10;
         int generation = 1;
+        private object label1;
+        private IEnumerable<object> topPerformers;
+
         public Form1()
         {
             InitializeComponent();
@@ -28,6 +31,40 @@ namespace gyak_10
             this.Controls.Add(ga);
             gc.AddPlayer();
             gc.Start(true);
+            gc.GameOver += Gc_GameOver;
+
+            for (int i = 0; i < populationSize; i++)
+            {
+                gc.AddPlayer(nbrOfSteps);
+            }
+            gc.Start();
+            var playerList = from p in gc.GetCurrentPlayers()
+                             orderby p.GetFitness() descending
+                             select p;
+            var topPerformers = playerList.Take(populationSize / 2).ToList();
+        }
+
+        private void Gc_GameOver(object sender)
+        {
+            generation++;
+            label2.Text = string.Format(
+                "{0}. generáció",
+                generation);
+            gc.ResetCurrentLevel();
+            foreach (var p in topPerformers)
+            {
+                var b = p.Brain.Clone();
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b);
+
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.Mutate().ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b.Mutate());
+            }
+            gc.Start();
         }
     }
 }
